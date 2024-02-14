3993f4140c0e13c8deecb115ca0d278ac1d1c066
Sergey Petrosyan <ipetriez@icloud.com>
2024-02-14 12:31:46 +0400

    Create SleepCalculator.mlmodel. Implement logic inside ContentView.calculateSleepTime().

--- BetterRest/MainScreen/ContentView.swift
+++ BetterRest/MainScreen/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Sergey Petrosyan on 14.02.24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,10 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showAlert = false
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -37,11 +42,31 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateSleepTime)
             }
+            .alert(alertTitle, isPresented: $showAlert) {
+                Button("Okay") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
     func calculateSleepTime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hoursInSeconds = (components.hour ?? 0) * 60 * 60
+            let minutesInSeconds = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Int64(hoursInSeconds + minutesInSeconds), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is:"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            print("DEBUG: Failed to create SleepCalculator from MLModelConfiguration with error: \(error)")
+        }
+        showAlert = true
     }
 }
 
