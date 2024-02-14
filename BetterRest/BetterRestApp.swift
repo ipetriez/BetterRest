4f35850090113af457e0cd462084855929af47f7
Sergey Petrosyan <ipetriez@icloud.com>
2024-02-14 11:10:20 +0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sergey Petrosyan on 14.02.24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
